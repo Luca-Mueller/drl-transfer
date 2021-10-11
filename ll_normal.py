953cde5f46e7f3e82394ad904789bb9d059f3bc2
Luca Mueller <d.luca.mueller@gmail.com>
2021-10-11 22:20:34 +0200

    added colored metrics

--- ll_normal.py
+++ ll_normal.py
@@ -1,5 +1,5 @@
 import gym
-from pathlib import Path
+from colorama import init, Fore, Style
 
 import torch
 import torch.nn as nn
@@ -12,8 +12,9 @@ from torch_agents.agent import DQNAgent, DDQNAgent, DQVAgent
 from torch_agents.plotting import plot_scores
 from torch_agents.utils import AgentArgParser
 
+# initialize color / gym / device
+init()
 env = gym.make('LunarLander-v2').unwrapped
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # parse args
@@ -23,17 +24,25 @@ args = arg_parser.parse_args()
 # agent type
 if args.agent == "DQN":
     agent_type = DQNAgent
+    agent_color = Fore.LIGHTCYAN_EX
 elif args.agent == "DDQN":
     agent_type = DDQNAgent
+    agent_color = Fore.LIGHTMAGENTA_EX
 elif args.agent == "DQV":
     agent_type = DQVAgent
+    agent_color = Fore.LIGHTYELLOW_EX
 else:
     print("Error: invalid agent type")
     exit(-1)
 
+print("Agent:\t" + agent_color + args.agent + Style.RESET_ALL)
+
 # device
 if args.cpu:
     device = torch.device("cpu")
+
+device_color = Fore.LIGHTBLUE_EX if str(device) == "cpu" else Fore.LIGHTGREEN_EX
+print("Device:\t" + device_color + str(device).upper() + Style.RESET_ALL + "\n")
 
 # hyperparameters
 LR = args.lr
@@ -47,6 +56,20 @@ N_EPISODES = args.episodes
 MAX_STEPS = args.max_steps
 WARM_UP = args.warm_up
 TARGET_UPDATE = args.target_update
+
+param_color = Fore.YELLOW
+print("Agent Hyperparameters:")
+print(f"* Learning Rate:  {param_color}{LR}{Style.RESET_ALL}")
+print(f"* Batch Size:     {param_color}{BATCH_SIZE}{Style.RESET_ALL}")
+print(f"* Buffer Size:    {param_color}{BUFFER_SIZE}{Style.RESET_ALL}")
+print(f"* Gamma:          {param_color}{GAMMA}{Style.RESET_ALL}")
+print(f"* Eps Start:      {param_color}{EPS_START}{Style.RESET_ALL}")
+print(f"* Eps End:        {param_color}{EPS_END}{Style.RESET_ALL}")
+print(f"* Eps Decay:      {param_color}{EPS_DECAY}{Style.RESET_ALL}")
+print(f"* Episodes:       {param_color}{N_EPISODES}{Style.RESET_ALL}")
+print(f"* Max Steps:      {param_color}{MAX_STEPS}{Style.RESET_ALL}")
+print(f"* Warm Up:        {param_color}{WARM_UP}{Style.RESET_ALL}")
+print(f"* Target Update:  {param_color}{TARGET_UPDATE}{Style.RESET_ALL}\n")
 
 # visualization
 VIS_TRAIN = args.vv
@@ -66,24 +89,18 @@ policy = EpsilonGreedyPolicy(model, device, eps_start=EPS_START, eps_end=EPS_END
 agent = agent_type(model, replay_buffer, policy, optimizer, loss_function, gamma=GAMMA,
                    target_update_period=TARGET_UPDATE, device=device)
 
-print("Agent: " + agent.name + "\n")
-
 # Train
 print("Train...")
 episode_scores = agent.train(env, N_EPISODES, MAX_STEPS, batch_size=BATCH_SIZE, warm_up_period=WARM_UP,
                              visualize=VIS_TRAIN)
-print("Training complete\n")
 
-fig = plot_scores(episode_scores, title=("LunarLander-v2 " + agent.name + " Training"))
-plot_dir = Path("plots")
-fig.savefig(plot_dir / ("ll_v2_" + agent.name))
+print(Fore.GREEN + "Done\n" + Style.RESET_ALL)
+plot_scores(episode_scores, title=("LunarLander-v2 " + agent.name + " Training"))
 
 # Test
 print("Evaluate...")
 print(f"Target Score: {env.spec.reward_threshold:.2f}")
 test_scores = agent.play(env, 100, env.spec.max_episode_steps, visualize=VIS_EVAL)
-print("Evaluation complete")
 
-fig = plot_scores(test_scores, title=("LunarLander-v2 " + agent.name + " Test"))
-plot_dir = Path("plots")
-fig.savefig(plot_dir / ("ll_v2_test_" + agent.name))
+print(Fore.GREEN + "Done\n" + Style.RESET_ALL)
+plot_scores(test_scores, title=("LunarLander-v2 " + agent.name + " Test"))
