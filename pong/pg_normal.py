af07413274c31c96be7cea373e900db97cda2833
Luca Mueller <d.luca.mueller@gmail.com>
2021-11-30 11:59:04 +0100

    modified ALE models

--- pong/pg_normal.py
+++ pong/pg_normal.py
@@ -1,4 +1,6 @@
 import gym
+import numpy as np
+import collections
 from colorama import init, Fore, Style
 
 import torch
@@ -12,9 +14,56 @@ from torch_agents.agent import DQNAgent, DDQNAgent, DQVAgent, DQV2Agent
 from torch_agents.plotting import plot_scores
 from torch_agents.utils import AgentArgParser, ArgPrinter
 
+
+class FireResetEnv(gym.Wrapper):
+    def __init__(self, env=None):
+       super(FireResetEnv, self).__init__(env)
+       assert env.unwrapped.get_action_meanings()[1] == 'FIRE'
+       assert len(env.unwrapped.get_action_meanings()) >= 3
+
+    def step(self, action):
+       return self.env.step(action)
+
+    def reset(self):
+       self.env.reset()
+       obs, _, done, _ = self.env.step(1)
+       if done:
+          self.env.reset()
+       obs, _, done, _ = self.env.step(2)
+       if done:
+          self.env.reset()
+       return obs
+
+
+class MaxAndSkipEnv(gym.Wrapper):
+    def __init__(self, env=None, skip=4):
+        super(MaxAndSkipEnv, self).__init__(env)
+        self._obs_buffer = collections.deque(maxlen=2)
+        self._skip = skip
+
+    def step(self, action):
+        total_reward = 0.0
+        done = None
+        for _ in range(self._skip):
+           obs, reward, done, info = self.env.step(action)
+           self._obs_buffer.append(obs)
+           total_reward += reward
+           if done:
+               break
+        max_frame = np.max(np.stack(self._obs_buffer), axis=0)
+        return max_frame, total_reward, done, info
+
+    def reset(self):
+       self._obs_buffer.clear()
+       obs = self.env.reset()
+       self._obs_buffer.append(obs)
+       return obs
+
+
 # initialize color / gym / device
 init()
-env = gym.make('Pong-v0').unwrapped
+env = gym.make('Pong-v0')#.unwrapped
+env = FireResetEnv(MaxAndSkipEnv(env))
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # parse args
@@ -87,9 +136,8 @@ plot_scores(episode_scores, title=("Pong-v0 " + agent.name + " Training"))
 # test
 print("Evaluate...")
 threshold = env.spec.reward_threshold
-if threshold is None:
-    threshold = 0.0
-print(f"Target Score: {threshold:.2f}")
+if threshold:
+    print(f"Target Score: {threshold:.2f}")
 test_scores = agent.play(env, 100, env.spec.max_episode_steps, visualize=VIS_EVAL)
 
 print(Fore.GREEN + "Done\n" + Style.RESET_ALL)
--- torch_agents/models.py
+++ torch_agents/models.py
@@ -74,21 +74,21 @@ class ConvDQN(nn.Module):
     def __init__(self, h: int, w: int, outputs: int, window_size: int = 4):
         super(ConvDQN, self).__init__()
         self.shape = ((window_size, h, w), outputs)
-        self.conv1 = nn.Conv2d(window_size, 16, kernel_size=5, stride=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(32)
+        self.conv1 = nn.Conv2d(window_size, 32, kernel_size=8, stride=4)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.bn3 = nn.BatchNorm2d(64)
 
         # Number of Linear input connections depends on output of conv2d layers
         # and therefore the input image size, so compute it.
         def conv2d_size_out(size, kernel_size=5, stride=2):
             return (size - (kernel_size - 1) - 1) // stride + 1
 
-        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
-        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
-        linear_input_size = convw * convh * 32
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w, 8, 4), 4, 2), 3, 1)
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h, 8, 4), 4, 2), 3, 1)
+        linear_input_size = convw * convh * 64
         self.head = nn.Linear(linear_input_size, outputs)
 
     def forward(self, x):
@@ -109,21 +109,21 @@ class ConvDQN(nn.Module):
 class ConvVModel(nn.Module):
     def __init__(self, h: int, w: int, window_size: int = 4):
         super(ConvVModel, self).__init__()
-        self.conv1 = nn.Conv2d(window_size, 16, kernel_size=5, stride=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(32)
+        self.conv1 = nn.Conv2d(window_size, 32, kernel_size=8, stride=4)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.bn3 = nn.BatchNorm2d(64)
 
         # Number of Linear input connections depends on output of conv2d layers
         # and therefore the input image size, so compute it.
         def conv2d_size_out(size, kernel_size=5, stride=2):
             return (size - (kernel_size - 1) - 1) // stride + 1
 
-        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
-        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
-        linear_input_size = convw * convh * 32
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w, 8, 4), 4, 2), 3, 1)
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h, 8, 4), 4, 2), 3, 1)
+        linear_input_size = convw * convh * 64
         self.head = nn.Linear(linear_input_size, 1)
 
     def forward(self, x):
@@ -151,10 +151,8 @@ class ConvV2Model(nn.Module):
         # and therefore the input image size, so compute it.
         def conv2d_size_out(size, kernel_size=5, stride=2):
             return (size - (kernel_size - 1) - 1) // stride + 1
-        h, w = conv_dqn.shape[0][1], conv_dqn[0][2]
-        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
-        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
-        linear_input_size = convw * convh * 32
+
+        linear_input_size = conv_dqn.head.in_features
         self.head = nn.Linear(linear_input_size, 1)
 
     def forward(self, x):
